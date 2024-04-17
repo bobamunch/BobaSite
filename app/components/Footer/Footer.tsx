a0ea6456e5714bc8e8e45049b042baa03243d640
Kevin Huang <k.huang0312@gmail.com>
2024-04-17 00:11:02 -0700

    Got icons in footer to link properly to socials

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -2,32 +2,36 @@ import React from "react";
 
 import pkg from "grommet-icons";
 
-import FetlifeIcon from "../FetlifeIcon/FetlifeIcon";
+import FetlifeIcon from "../Icons/FetlifeIcon";
 import { InstagramLogoIcon, EnvelopeClosedIcon } from "@radix-ui/react-icons";
+import { redirect } from "@remix-run/node";
+import { Link } from "@remix-run/react";
 
 function Footer() {
   return (
     <footer>
       <div className="footer__icons">
-        <InstagramLogoIcon
-          height={26}
-          width={26}
-          href="https://www.instagram.com/bobamunch/"
-          className="footer__icon"
-        />
-        <FetlifeIcon
-          height={26}
-          width={26}
-          fill={"#E5A363"}
-          href="https://fetlife.com/users/14009315"
-          className="footer__icon"
-        />
-        <EnvelopeClosedIcon
-          height={26}
-          width={26}
-          href="bobamunch@gmail.com"
-          className="footer__icon"
-        />
+        <Link to="https://www.instagram.com/bobamunch/">
+          <InstagramLogoIcon
+            height={26}
+            width={26}
+            className="footer__icon"
+            href="https://www.instagram.com/bobamunch/"
+          />
+        </Link>
+
+        <Link to="https://fetlife.com/users/14009315">
+          <FetlifeIcon
+            height={26}
+            width={26}
+            fill={"#E5A363"} // needed to keep it orange
+            className="footer__icon"
+          />
+        </Link>
+
+        <Link to="mailto:bobamunch@gmail.com">
+          <EnvelopeClosedIcon height={26} width={26} className="footer__icon" />
+        </Link>
       </div>
       <small className="">
         Copyright ©{new Date().getFullYear()} BOBA Munch
